4e5053e4e6b80ef3fd326503a4d64cd86268f887
Gino Prasad <ginoprasad@gmail.com>
2022-07-21 20:52:07 -0700

    Creates an Automated Task Selecting System using Crontab and AlarmSetter Repository

---
+++ main.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import csv
+import settings
+import random
+import sys
+import subprocess as sp
+import datetime
+import os
+
+
+def get_tasks():
+	with open(settings.tasks_path) as tasks_file:
+		tasks = list(csv.reader(tasks_file))
+	return tasks
+
+
+def update_tasks(tasks):
+	with open(settings.tasks_path, 'w') as tasks_file:
+		csv.writer(tasks_file).writerows(tasks)
+
+
+def get_sum():
+	with open(settings.sum_path) as infile:
+		return int(infile.read().strip())
+
+
+def update_sum(new_sum):
+	with open(settings.sum_path, 'w') as outfile:
+		outfile.write(str(new_sum))
+
+
+def update_gravy(task):
+	with open(settings.gravy_path, 'a') as outfile:
+		outfile.write(f"{task}\n")
+
+
+def decrement_task(tasks, index):
+	task = tasks[index]
+	if task[1] == '1':
+		update_gravy(tasks.pop(index)[0])
+	else:
+		task[1] = str(int(task[1]) - 1)
+	update_tasks(tasks)
+	return task[0]
+
+
+def get_new_task_info():
+	name = ""
+	units = ""
+
+	print()
+	while not name:
+		name = input(settings.input_name)
+	while not units.isnumeric():
+		units = input(settings.input_units)
+	print("\n")
+
+	return name, int(units)
+
+
+def add_task():
+	name, units = get_new_task_info()
+	tasks = get_tasks()
+	tasks.append([name, str(units)])
+	update_tasks(tasks)
+	update_sum(get_sum() + units)
+
+
+def list_tasks():
+	tasks = get_tasks()
+
+	if not tasks:
+		print(settings.no_task_message)
+		exit()
+
+	print(settings.task_column_labels)
+	for task in tasks:
+		print(settings.task_string.format(task[0]))
+
+
+
+def pick_task():
+	tasks = get_tasks()
+	tasks_sum = get_sum()
+	task = random.randint(1, tasks_sum)
+
+	picked_task = -1
+	while task > 0:
+		picked_task += 1
+		task -= int(tasks[picked_task][1])
+	
+	update_sum(tasks_sum - 1)
+	return decrement_task(tasks, picked_task)
+
+
+def check_time():
+	with open(settings.time_file) as infile:
+		time = datetime.datetime.strptime(infile.read().strip(), settings.time_format)
+	diff = (datetime.datetime.now() - time).seconds // 60
+	return diff >= int(settings.time_designated)
+
+
+def set_time():
+	with open(settings.time_file, 'w') as outfile:
+		outfile.write(datetime.datetime.now().strftime(settings.time_format))
+
+
+def main():
+	if check_time():
+		task = pick_task()
+		if task:
+			sp.run([settings.alarm_setter_path, "set", 
+				settings.time_until_break, 
+				settings.break_message.format(task)])
+			sp.run([settings.alarm_setter_path, "set", 
+				settings.time_designated, 
+				settings.break_over_message])
+			print(settings.next_task_message.format(task))
+			set_time()
+		else:
+			print(settings.no_task_message)
+	else:
+		print(settings.keep_working_message)
+
+
+
+if __name__ == "__main__":
+	assert len(sys.argv)
+	os.chdir(os.path.dirname(sys.argv[0]))
+
+	command = ''.join(sys.argv[1:])
+	if command == "next":
+		main()
+	elif command == "add":
+		add_task()
+	elif command == "list":
+		list_tasks()
+	else:
+		raise AssertionError(settings.invalid_syntax_message)
+
---
+++ settings.py
@@ -0,0 +1,24 @@
+# File Paths
+alarm_setter_path = "/Users/ginoprasad/Scripts/AlarmSetter/main.sh"
+tasks_path = "text_files/tasks.csv"
+sum_path = "text_files/sum.txt"
+gravy_path = "text_files/gravy.txt"
+time_file = "text_files/time.txt"
+
+# Presets
+time_format = "%m/%d/%Y, %H:%M:%S"
+time_designated = "25"
+time_until_break = "20"
+
+input_name = "\n\tEnter Name: "
+input_units = "\n\tUnits: "
+
+next_task_message = "\n\tNext Task: {}\n"
+break_message = "Take a break!!!"
+break_over_message = "Break Over!!!"
+no_task_message = "\n\tNo Tasks Left :)\n"
+keep_working_message = "\n\tWoah There Buddy, Still on the Clock\n"
+invalid_syntax_message = "ERROR: INVALID SYNTAX"
+
+task_column_labels = "\n\tTASKS REMAINING:\n"
+task_string = "\t\t{}\n"
